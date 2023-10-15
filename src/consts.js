d50fb189f1d37751c8e7843c40bf69c82fc348fe
Seth Livingston <seth.livingston@proton.me>
2023-10-15 11:01:35 -0500

    Initial work on front page

---
+++ src/consts.js
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Everything I know about software development";
+export const SITE_DESCRIPTION = "by Seth Livingston, software engineer ever since I got my first computer in 5th grade";
---
+++ tailwind.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    fontFamily: {
+      title: "Londrina Solid, sans-serif",
+      sans: "Schibsted Grotesk, sans-serif" 
+    },
+    extend: {},
+  },
+  plugins: [],
+}
