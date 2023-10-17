edfdf89c3c18e7e054a627dfc37bc3c9d63f12b2
Seth Livingston <seth.livingston@proton.me>
2023-10-17 16:14:30 -0500

    Tweak Vercel configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,13 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap(), tailwind()],
-  output: "server",
+  output: "static",
   adapter: vercel({
     speedInsights: { enabled: true },
     webAnalytics: { enabled: true}
